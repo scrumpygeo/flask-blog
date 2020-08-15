da07796fb2e459cdc87b4e213a5485eaa7714e4e
scrumpygeo <scrumpygeo@yahoo.ca>
2020-08-15 10:38:02 +0100

    fix error code typo

--- flaskblog/errors/handlers.py
+++ flaskblog/errors/handlers.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 
-@errors.app_errorhandler(500)
+@errors.app_errorhandler(404)
 def error_404(error):
     # in Flask u can return a 2nd value that is the status code (default is 200):
     return render_template('errors/404.html'), 404
